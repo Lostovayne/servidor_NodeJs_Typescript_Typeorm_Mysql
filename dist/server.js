039cc51a7a4d1e15e54a2eb58a0007434b365abc
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-05-26 22:38:02 -0400

    termino_clase_1

---
+++ dist/server.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+class ServerBootstrap {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.port = 8000;
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: true }));
+        this.app.use((0, morgan_1.default)('dev'));
+        this.app.use((0, cors_1.default)());
+        this.listen();
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        });
+    }
+}
+new ServerBootstrap();
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
 
 class ServerBootstrap {
-    
-    public app: express.Application = express();
-    private port : number = 8000;
-    
-    public listen(){
+    public app: express.Application = express()
+    private port: number = 8000
+
+    constructor() {
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+        this.app.use(morgan('dev'))
+        this.app.use(cors())
+        this.listen()
+    }
+
+    public listen() {
         this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en el puerto ${this.port}`);
-        });
+            console.log(`Servidor corriendo en el puerto ${this.port}`)
+        })
     }
 }
 
-
-
+new ServerBootstrap()
